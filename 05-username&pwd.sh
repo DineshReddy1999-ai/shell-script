f19c33dbbbf5027ff8955658bea31f160f65d631
DineshReddyKotte <dineshreddykt@gmail.com>
2025-04-25 07:02:57 +0530

    runtime args added

--- 05-username&pwd.sh
+++ 05-username&pwd.sh
@@ -3,6 +3,6 @@
 echo "please enter your username ::"
 
 read -s USERNAME
-echo "$USERNAME entered ::"
+echo "username entered is :: $USERNAME"
 echo "please enter your password ::"
 read -S PASSWORD

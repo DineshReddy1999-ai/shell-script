e9f19b047d4e86d4c1c93122fce02ea2dd911d3f
DineshReddyKotte <dineshreddykt@gmail.com>
2025-04-25 07:35:45 +0530

    runtime args added

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Bahubali" "RRR" "Devara")
+
+echo "First movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[0]}"
+echo "Third movie is ${MOVIES[0]}"
+
+echo "All movies are ${MOVIES[@]}"

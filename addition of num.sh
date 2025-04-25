6886f91375091d4d3d1e0b3a438bd624c7fe9972
DineshReddyKotte <dineshreddykt@gmail.com>
2025-04-25 07:26:29 +0530

    runtime args added

--- addition of num.sh
+++ addition of num.sh
@@ -3,8 +3,8 @@
 NUMBER1=$1
 NUMBER2=$2
 
-SUM = $(NUMBER1+NUMBER2)
+SUM=$((NUMBER1+NUMBER2))
 
 echo "sum of $NUMBER1 and $NUMBER2  is :: $SUM"
-TIMESTAMP = $(date)
+TIMESTAMP=$(date)
 echo "addition operation executed at :: $TIMESTAMP"

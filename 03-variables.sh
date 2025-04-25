e5de90dae72316edb0224e341e36d2eeb482b398
DineshReddyKotte <dineshreddykt@gmail.com>
2025-04-25 06:47:26 +0530

    vars script added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Dhoni
+PERSON2=Dinesh
+
+
+echo "$PERSON1  : Hi $PERSON2."
+echo "$PERSON2  : Hello $PERSON1."
+echo "$PERSON1  : Nice to meet You."
+echo "$PERSON2  : Glad to meet you."
